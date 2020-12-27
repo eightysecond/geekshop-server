c22f946059630beaad20f24a9d014aa947e075b7
eightysecond <eightysecond@rambler.ru>
2020-12-27 22:18:03 +1100

    Задание лекции 8

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import user_passes_test
+from django.utils.decorators import method_decorator
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 
 from authapp.models import User
 from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm
@@ -11,50 +15,95 @@ def index(request):
     return render(request, "adminapp/index.html")
 
 
-# Следующие контроллеры демонстрируют принцип CRUD
-@user_passes_test(lambda u: u.is_superuser)
-def admin_users_create(request):
-    # C - Create
-    if request.method == "POST":
-        form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse("admin_staff:admin_users"))
-        else:
-            print(form.errors)
-    else:
-        form = UserAdminRegisterForm()
-    context = {"form": form}
-    return render(request, "adminapp/admin-users-create.html", context)
+class UserCreateView(CreateView):
+    model = User
+    template_name = "adminapp/admin-users-create.html"
+    success_url = reverse_lazy("adminapp:admin_users")
+    form_class = UserAdminRegisterForm
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserCreateView, self).dispatch(request, *args, **kwargs)
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def admin_users(request):
-    # R - Read
-    context = {
-        "users": User.objects.all(),
-    }
-    return render(request, "adminapp/admin-users-read.html", context)
+# # Следующие контроллеры демонстрируют принцип CRUD
+# @user_passes_test(lambda u: u.is_superuser)
+# def admin_users_create(request):
+#     # C - Create
+#     if request.method == "POST":
+#         form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse("admin_staff:admin_users"))
+#         else:
+#             print(form.errors)
+#     else:
+#         form = UserAdminRegisterForm()
+#     context = {"form": form}
+#     return render(request, "adminapp/admin-users-create.html", context)
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def admin_users_update(request, user_id):
-    user = User.objects.get(id=user_id)
-    if request.method == "POST":
-        form = UserAdminProfileForm(data=request.POST, files=request.FILES, instance=user)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse("admin_staff:admin_users"))
-    else:
-        form = UserAdminProfileForm(instance=user)
+class UserListView(ListView):
+    model = User
+    template_name = "adminapp/admin-users-read.html"
 
-    context = {"form": form, "user": user}
-    return render(request, "adminapp/admin-users-update-delete.html", context)
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserListView, self).dispatch(request, *args, **kwargs)
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def admin_users_remove(request, user_id):
-    user = User.objects.get(id=user_id)
-    user.is_active = False
-    user.save()
-    return HttpResponseRedirect(reverse("admin_staff:admin_users"))
+# @user_passes_test(lambda u: u.is_superuser)
+# def admin_users(request):
+#     # R - Read
+#     context = {
+#         "users": User.objects.all(),
+#     }
+#     return render(request, "adminapp/admin-users-read.html", context)
+
+class UserUpdateView(UpdateView):
+    model = User
+    template_name = "adminapp/admin-users-update-delete.html"
+    success_url = reverse_lazy("adminapp:admin_users")
+    form_class = UserAdminProfileForm
+
+    def get_context_data(self, **kwargs):
+        context = super(UserUpdateView, self).get_context_data(**kwargs)
+        context["title"] = "GeekShop - Редактирование пользователя"
+        return context
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserUpdateView, self).dispatch(request, *args, **kwargs)
+
+# @user_passes_test(lambda u: u.is_superuser)
+# def admin_users_update(request, user_id):
+#     user = User.objects.get(id=user_id)
+#     if request.method == "POST":
+#         form = UserAdminProfileForm(data=request.POST, files=request.FILES, instance=user)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse("admin_staff:admin_users"))
+#     else:
+#         form = UserAdminProfileForm(instance=user)
+#
+#     context = {"form": form, "user": user}
+#     return render(request, "adminapp/admin-users-update-delete.html", context)
+
+
+class UserDeleteView(DeleteView):
+    model = User
+    template_name = "adminapp/admin-users-update-delete.html"
+    success_url = reverse_lazy("adminapp:admin_users")
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.is_active = False
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+# @user_passes_test(lambda u: u.is_superuser)
+# def admin_users_remove(request, user_id):
+#     user = User.objects.get(id=user_id)
+#     user.is_active = False
+#     user.save()
+#     return HttpResponseRedirect(reverse("admin_staff:admin_users"))
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from mainapp.models import Product, ProductCategory
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def main(request):
@@ -9,10 +10,18 @@ def main(request):
     return render(request, "mainapp/index.html", content)
 
 
-def products(request, id=None):
-    content = {
-        "title": "GeekShop - Категории",
-        "categories": ProductCategory.objects.all(),
-        "products": Product.objects.all(),
-    }
-    return render(request, "mainapp/products.html", content)
+def products(request, category_id=None, page=1):
+    context = {"title": "GeekShop - Категории", "categories": ProductCategory.objects.all()}
+    if category_id:
+        products = Product.objects.filter(category_id=category_id).order_by("price")
+    else:
+        products = Product.objects.all().order_by("price")
+    paginator = Paginator(products, 3)
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+    context.update({"products": products_paginator})
+    return render(request, "mainapp/products.html", context)
